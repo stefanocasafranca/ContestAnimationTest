a79d6cedc39e8a7ada76bcc62902bb3c3dbb1fb0
stefanocasafranca <scasafrancal01@gmail.com>
2025-04-20 22:28:33 -0500

    ShakingNoticeable

--- Contest/ViewController.swift
+++ Contest/ViewController.swift
@@ -14,6 +14,44 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    func shakeTextField(_ textField: UITextField) {
+        let shake: CGFloat = 1000
 
+        UIView.animateKeyframes(withDuration: 0.4, delay: 0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.1) {
+                textField.transform = CGAffineTransform(translationX: -shake, y: 0)
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.1) {
+                textField.transform = CGAffineTransform(translationX: shake, y: 0)
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.1) {
+                textField.transform = CGAffineTransform(translationX: -shake, y: 0)
+            }
+            // Reset to normal position
+            UIView.addKeyframe(withRelativeStartTime: 0.3, relativeDuration: 0.1) {
+                textField.transform = .identity
+            }
+        })
+    }
+
+    
+    
+    
+    @IBAction func submitButton(_ sender: Any) {
+        guard let email = emailTextField.text, !email.isEmpty else {
+                shakeTextField(emailTextField) // Call the shake animation
+                return
+            }
+
+        // Identifier set to the arrow in the Main Storyboard
+            performSegue(withIdentifier: "goToSuccessScreen", sender: self)
+        
+        
+    }
+    
+    
+    
 }
 
